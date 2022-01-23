0ee31b9dc9c1faf11cec41d8b781006bef7d67c3
Tomas Petrikas <tomas.petrikas99@gmail.com>
2022-01-23 09:25:40 +0200

    Implement opponent AI

--- computer_player.rb
+++ computer_player.rb
@@ -9,9 +9,27 @@ class ComputerPlayer < Player
     super
   end
 
-  # TODO: make this actually good
-  def guess(_board)
-    generate_code
+  def guess(board)
+    # Return a random guess on first turn
+    if board.current_turn.zero?
+      @codes = generate_all_codes
+      return generate_code
+    end
+
+    previous_guess = board.board[board.current_turn - 1].clone
+    previous_clues = board.clues[board.current_turn - 1].clone
+    @codes.delete(previous_guess)
+
+    @codes.each_with_index do |c, i|
+      @codes[i] = nil unless board.generate_clues(c.clone, previous_guess.clone) == previous_clues
+    end
+
+    @codes.delete(nil)
+
+    guess = @codes.sample.join
+    raise "Error: guess '#{guess}' invalid" unless Board.valid?(guess)
+
+    guess
   end
 
   private
@@ -23,5 +41,9 @@ class ComputerPlayer < Player
     end
 
     code.join
+  end
+
+  def generate_all_codes
+    Board::CODE_SYMBOLS.repeated_permutation(Board::CODE_LENGTH).to_a
   end
 end
